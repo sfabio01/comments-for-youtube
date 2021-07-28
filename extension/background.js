8073526c385ade0bf66f07425afdcc3d18fa094d
Fabio Sabbion <fabiosabbion.apps@gmail.com>
2021-07-28 14:23:03 +0200

    removed 'welcome' screen

--- extension/background.js
+++ extension/background.js
@@ -1,11 +1,3 @@
-chrome.runtime.onInstalled.addListener((reason) => {
-    if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
-        chrome.tabs.create({
-            url: 'welcome.html'
-        });
-    }
-});
-
 chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
     if (tab.url.startsWith("https://www.youtube.com/watch?v=")) {
         chrome.action.enable(tabId);
