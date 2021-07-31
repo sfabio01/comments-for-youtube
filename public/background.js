315b7067fc80729bc22e1530c55abbf7fead76f6
Fabio Sabbion <fabiosabbion.apps@gmail.com>
2021-07-31 11:11:31 +0200

    migrated to Manifest V3

--- public/background.js
+++ public/background.js
@@ -1,8 +1,8 @@
 // EXTENSION ACTIVATION 
 chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
     if (tab.url.startsWith("https://www.youtube.com/watch?v=")) {
-        chrome.browserAction.enable(tabId);
+        chrome.action.enable(tabId);
     } else {
-        chrome.browserAction.disable(tabId);
+        chrome.action.disable(tabId);
     }
 });
